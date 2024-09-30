31ce5a637df6a322fa7a59d084347941792533fb
SCate <silence.bz@gmail.com>
2024-09-30 22:27:55 +0800

    feat: 修改文案&图标

--- app/store/update.ts
+++ app/store/update.ts
@@ -104,7 +104,7 @@ export const useUpdateStore = createPersistStore(
                       if (version === remoteId) {
                         // Show a notification using Tauri
                         window.__TAURI__?.notification.sendNotification({
-                          title: "NextChat",
+                          title: "AIWUS",
                           body: `${Locale.Settings.Update.IsLatest}`,
                           icon: `${ChatGptIcon.src}`,
                           sound: "Default",
@@ -114,7 +114,7 @@ export const useUpdateStore = createPersistStore(
                           Locale.Settings.Update.FoundUpdate(`${remoteId}`);
                         // Show a notification for the new version using Tauri
                         window.__TAURI__?.notification.sendNotification({
-                          title: "NextChat",
+                          title: "AIWUS",
                           body: updateMessage,
                           icon: `${ChatGptIcon.src}`,
                           sound: "Default",
